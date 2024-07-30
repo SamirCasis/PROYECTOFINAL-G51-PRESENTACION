91ec9bb49fb50f8bd651d5712c6b8a1ee40a434d
the_p <the_punisher_deep@hotmail.com>
2024-07-30 00:43:42 -0400

    agregando views

--- CLIENT/src/components/Logued.jsx
+++ CLIENT/src/components/Logued.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Logued.css'
+import { Link } from 'react-router-dom'
 
 const Logued = () => {
   return (
-    <h1 className='h1Logued'>Hola mundo</h1>
+    <main className='userMain'>
+<section className='userPanel'>
+    <h1>DATOS PERSONALES</h1>
+   <article>
+        <Link className='options'>ACTUALIZA TUS DATOS</Link>
+        <Link className='options'>REVISA TUS FAVORITOS</Link>
+   </article>
+</section>
+<section>
+
+</section>
+    </main>
   )
 }
 
