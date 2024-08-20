cf7f1771d9afb167a8c908067d32edf25a6e99ce
the_p <the_punisher_deep@hotmail.com>
2024-08-19 22:28:00 -0400

    ruta front

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -13,8 +13,8 @@ const App = () => {
       <Routes>
         {/* Rutas Públicas */}
         <Route path='/' element={<HomeView />} />
-        <Route path='/register' element={<RegisterView />} />
-        <Route path='/login' element={<LoginView />} />
+        <Route path='https://proyectofinal-g51-presentacion.onrender.com/register' element={<RegisterView />} />
+        <Route path='https://proyectofinal-g51-presentacion.onrender.com/login' element={<LoginView />} />
         <Route path='*' element={<NotFound />} />
         {/* Rutas Protegidas */}
         <Route path='/usersesion/*' element={<ProtectedRoute element={UserView} requiredRole="user" />} />
